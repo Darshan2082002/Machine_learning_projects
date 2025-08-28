03f4507037b1d7f066020aa49ebc85d89fea73fe
Darshan2082002 <darshanbalu20@gmail.com>
2025-08-28 18:49:38 +0530

    No changes

--- wine_prediction.py
+++ wine_prediction.py
@@ -2,3 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 import sklearn.metrics
+from sklearn.model_selection import train_test_split
+import seaborn as sns 
+from sklearn.metrics import  accuracy_score, confusion_matrix, classification_report
+from sklearn.
