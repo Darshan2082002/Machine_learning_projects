57e900c8f2d2b03524a01e20da778da889a8089e
Darshan2082002 <darshanbalu20@gmail.com>
2025-08-28 18:56:52 +0530

    creating

--- wine_prediction.py
+++ wine_prediction.py
@@ -1,8 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
-import sklearn.metrics
-from sklearn.model_selection import train_test_split
 import seaborn as sns 
-from sklearn.metrics import  accuracy_score, confusion_matrix, classification_report
-from sklearn.
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.linear_model import LinearRegression
+import streamlit as slt
+
+df = pd.read_csv("WineQT.csv")
+print("Data info:")
+print(df.describe(8*13))
+print(df.head())
