416373ab779d37cd228b7e9c5afde1d5f7c53703
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-07 21:58:45 +0530

    Changes to be committed: f

---
+++ CIFAR10.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from tensorflow.keras.datasets import cifar10
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+print("training data shape:", X_train.shape, y_train.shape)
+
+plt.imshow(X_train[1])
+plt.title(f"Label: {y_train[1]}")
+plt.show()
