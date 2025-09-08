5ed196a7ad9eb95a641c1bee071bf03d85ec7cd8
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-08 10:23:55 +0530

    Working with different datasets import numpy as np import matplotlib.pyplot as plt

--- CIFAR10.py
+++ CIFAR10.py
@@ -5,8 +5,8 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-from tensorflow.keras.datasets import cifar10
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+from tensorflow.keras.datasets import mnist
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
 print("training data shape:", X_train.shape, y_train.shape)
 
 plt.imshow(X_train[1])
