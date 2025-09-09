3dc966486e431dbe09755f3e7893b956c2e3598f
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-09 22:58:46 +0530

    Change the file name to Dog_cat_predictions.py

--- Dog_cat_predicitons.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-import pandas as pd 
-import matplotlib.pyplot as plt
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-from tensorflow.keras.datasets import mnist
-(X_train, y_train), (X_test, y_test) = 
