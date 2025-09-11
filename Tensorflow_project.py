d1a1a3961e38cbb8936606b054bf6b1ee0cb5999
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-11 22:30:22 +0530

    Changes made to the file to improve model accuracy.

--- Tensorflow_project.py
+++ Tensorflow_project.py
@@ -2,4 +2,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.datasets import mnist
+from tensorflow.keras import datasets
+
+(x_train,y_train),(x_test,y_test)=datasets.fashion_mnist.load_data()
+
+plt.imshow(x_train[0],cmap='gray')
+plt.title(y_train[0])
+plt.show()
+
+x_train=x_train/255.0
+x_test=x_test/255.0
