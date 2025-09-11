52d1a5361947badf62377b534b161af2e08fd916
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-11 22:36:18 +0530

    Changes made to Tensorflow_project.py to improve model accuracy and add data augmentation.

--- Tensorflow_project.py
+++ Tensorflow_project.py
@@ -12,3 +12,9 @@ plt.show()
 
 x_train=x_train/255.0
 x_test=x_test/255.0
+
+x_train=x_train.reshape((-1,28,28,1))
+x_test=x_test.reshape((-1,28,28,1))
+
+print("Training data shape:", x_train.shape, y_train.shape)
+print("Testing data shape:", x_test.shape, y_test.shape)
