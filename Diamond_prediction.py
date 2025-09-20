2a932f50d97136a4f1878fd0b782b2a05eaa0211
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-20 14:08:21 +0530

    Data loading and intial exploration

--- Diamond_prediction.py
+++ Diamond_prediction.py
@@ -6,3 +6,5 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
+data=pd.read_csv("diamonds.csv")
+pritn(data.head())
