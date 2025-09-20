5e60f000dc3f916296fca9347bd3252c52682de7
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-20 14:33:56 +0530

    Deploy model to production

--- Diamond_prediction.py
+++ Diamond_prediction.py
@@ -5,6 +5,7 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+import streamlit as st
 
 data=pd.read_csv("diamonds.csv")
 print(data.head())
