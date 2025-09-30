050cb97ca77ec08e1c07a47a962bb00beb42556f
Darshan2082002 <darshanbalu20@gmail.com>
2025-10-01 00:31:24 +0530

    checking model accuracy

--- Insurance.py
+++ Insurance.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import classification_report,confusion_matrix, accuracy_score
 
 data=pd.read_csv("insurance.csv")
 print(data.columns)
