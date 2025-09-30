550a88143a38b1325c4489d449c0b708bf3979e8
Darshan2082002 <darshanbalu20@gmail.com>
2025-10-01 00:29:50 +0530

    Phase 3 completed

--- Insurance.py
+++ Insurance.py
@@ -17,3 +17,7 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=50
 model=LinearRegression()
 model.fit(X_train,y_train)
 y_pred=model.predict(X_test)
+
+print("Classification Report:\n",classification_report(y_test,y_pred))
+print("Confusion Matrix:\n",confusion_matrix(y_test,y_pred))
+print("Accuracy Score:",accuracy_score(y_test,y_pred)*100)
