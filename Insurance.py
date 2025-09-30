a52d1f707840dc055139dbde7db1af0b3a568b5a
Darshan2082002 <darshanbalu20@gmail.com>
2025-10-01 00:19:49 +0530

    Phase 2 work on progress

--- Insurance.py
+++ Insurance.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 data=pd.read_csv("insurance.csv")
 print(data.columns)
@@ -9,3 +11,8 @@ data['smoker']=data['smoker'].map({'yes':1, 'no':2})
 data['region']=data['region'].map({'southwest':1, 'southeast':2,'northwest':3, 'northeast':4})
 print(data.head)
 print(data.info)
+X=data.drop(columns=['charges'])
+y=data['charges']
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=50)
+model=LogisticRegression()
+model.fit()
