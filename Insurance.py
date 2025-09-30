47f0756ec9169c4812cf5cd5b1ac40646ae5f924
Darshan2082002 <darshanbalu20@gmail.com>
2025-10-01 00:29:22 +0530

    sssd

--- Insurance.py
+++ Insurance.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 
 data=pd.read_csv("insurance.csv")
 print(data.columns)
@@ -14,5 +14,6 @@ print(data.info)
 X=data.drop(columns=['charges'])
 y=data['charges']
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=50)
-model=LogisticRegression()
-model.fit()
+model=LinearRegression()
+model.fit(X_train,y_train)
+y_pred=model.predict(X_test)
