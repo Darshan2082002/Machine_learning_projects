131d93202dc1a5c9982e0b2909a48136bdaba3cc
Darshan2082002 <darshanbalu20@gmail.com>
2025-10-01 00:35:34 +0530

    MSE MSA R2

--- Insurance.py
+++ Insurance.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import classification_report,confusion_matrix, accuracy_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 data=pd.read_csv("insurance.csv")
 print(data.columns)
@@ -19,6 +19,6 @@ model=LinearRegression()
 model.fit(X_train,y_train)
 y_pred=model.predict(X_test)
 
-print("Classification Report:\n",classification_report(y_test,y_pred))
-print("Confusion Matrix:\n",confusion_matrix(y_test,y_pred))
-print("Accuracy Score:",accuracy_score(y_test,y_pred)*100)
+print("MSE:", mean_squared_error(y_test, y_pred))
+print("MAE:", mean_absolute_error(y_test, y_pred))
+print("R2 Score:", r2_score(y_test, y_pred))
