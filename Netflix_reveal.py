2c34784843661def257e445491697d4b0b3229a7
Darshan2082002 <darshanbalu20@gmail.com>
2025-10-06 21:53:45 +0530

    File View

--- Netflix_reveal.py
+++ Netflix_reveal.py
@@ -2,4 +2,5 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotenconder
+from sklearn.preprocessing import OneHotEncoder
+data=pd.read_csv()
