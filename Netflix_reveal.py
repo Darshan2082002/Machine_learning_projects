cdefbc0919622d660c9ae6ce60793113fa480494
Darshan2082002 <darshanbalu20@gmail.com>
2025-10-06 22:34:21 +0530

    update are on the way

--- Netflix_reveal.py
+++ Netflix_reveal.py
@@ -7,8 +7,25 @@ data=pd.read_csv('netflix_titles.csv', encoding='latin-1')
 print(data.info())
 print(data.head(5))
 
-df=pd.DataFrame(data)
-encoder=OneHotEncoder(sparse_output=False)
-encoder_features = encoder.fit_transform(df[['show_id', 'type', 'title', 'director', 'cast', 'country',
-                                             'date_added', 'release_year', 'rating', 'duration',
-                                             'listed_in', 'description']])
+df = pd.DataFrame(data)
+
+categorical_cols = ['type', 'country', 'rating', 'listed_in']
+
+r
+encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+
+encoder_features = encoder.fit_transform(df[categorical_cols])
+
+
+encoded_df = pd.DataFrame(
+    encoder_features,
+    columns=encoder.get_feature_names_out(categorical_cols)
+)
+
+final_df = pd.concat([df.drop(columns=categorical_cols), encoded_df], axis=1)
+
+
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
+print(final_df.head())
