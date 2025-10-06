52d34341f4b17dd05a5c0b101edd01f5cd523b24
Darshan2082002 <darshanbalu20@gmail.com>
2025-10-06 21:56:39 +0530

    Change are happening

--- Netflix_reveal.py
+++ Netflix_reveal.py
@@ -3,4 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
-data=pd.read_csv()
+data=pd.read_csv('netflix_titles.csv', encoding='latin-1')
+print(data.info())
+print(data.head(5))
