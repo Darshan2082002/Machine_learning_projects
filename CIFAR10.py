fcc484f0c3982719599d443bc74149e10cd07605
Darshan B <87748126+Darshan2082002@users.noreply.github.com>
2025-10-10 23:14:10 +0530

    Update CIFAR10.py

--- CIFAR10.py
+++ CIFAR10.py
@@ -6,9 +6,13 @@ from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from tensorflow.keras.datasets import mnist
+
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 print("training data shape:", X_train.shape, y_train.shape)
 
 plt.imshow(X_train[1])
 plt.title(f"Label: {y_train[1]}")
 plt.show()
+
+
+model=LinearRegression()
