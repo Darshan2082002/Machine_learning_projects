f9e8741b4c537f1195af951ea0fd28dc3de4e26d
Darshan2082002 <darshanbalu20@gmail.com>
2025-10-15 16:11:06 +0530

    Working on Machine learning project on Brain  rot disease prediction

--- Brainrot_maths.py
+++ Brainrot_maths.py
@@ -5,3 +5,7 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression 
 from sklearn.metrics import meann_squared_error,r2_score
+
+data=pd.read_csv('Brain Rot Cases (1).csv')
+print(data.head())
+print(data.describe())
