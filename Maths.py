9bb8abdc25cf83a6d001ba86444f4ee07bc6298d
Darshan2082002 <darshanbalu20@gmail.com>
2025-10-15 10:31:12 +0530

    Working on update value

--- Maths.py
+++ Maths.py
@@ -1,1 +1,7 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+x= np.array([[1], [2], [3], [4], [5]], dtype=float)
+y= np.array([[2], [4], [5], [4], [5]], dtype=float)
+plt.scatter(x,y)
+
