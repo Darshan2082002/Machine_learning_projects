ddf9dce69944dd3f5eec8d5080644c704dd62033
Darshan2082002 <darshanbalu20@gmail.com>
2025-10-15 16:06:29 +0530

    Brain rot maths analysis using Linear Regression

---
+++ Brainrot_maths.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd 
+import mathplot.pyplot as plt
+import seaborn as sns 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression 
+from sklearn.metrics import meann_squared_error,r2_score
