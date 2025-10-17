ad1275f3da01e4baca9d8fc267dfeec17c434636
Darshan B <87748126+Darshan2082002@users.noreply.github.com>
2025-10-17 23:27:07 +0530

    Add comment for data integration in Naives Bayes.py
    Added a comment regarding data integration for model training.

--- Naives Bayes.py
+++ Naives Bayes.py
@@ -2,3 +2,5 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 from sklearn.model_selection import train_test_split
+
+# data or file integration into  the system to train the model 
