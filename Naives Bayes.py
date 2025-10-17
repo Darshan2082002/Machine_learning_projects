1e8697a81c869188fd6b4e0437415bdfc9501773
Darshan B <87748126+Darshan2082002@users.noreply.github.com>
2025-10-17 23:30:36 +0530

    Update Naives Bayes.py

--- Naives Bayes.py
+++ Naives Bayes.py
@@ -7,3 +7,7 @@ from sklearn.model_selection import train_test_split
 data=pd.read_csv('NaivesBayes.csv')
 
 data.head() # to read the header of the  data to understand the target and other columns in it 
+x="train" # to check the data. if the model work properly 
+y=" test" # to predict the model is responding 
+
+(x_train,y_train,x_test,y_test= train_test_split(x,y,random_sixxe=0.2)
