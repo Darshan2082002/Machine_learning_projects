aed1c42c258012e712efda230aae589b4214f221
Darshan B <87748126+Darshan2082002@users.noreply.github.com>
2025-10-17 23:28:16 +0530

    Integrate dataset and display header
    Added code to read and display the header of the dataset.

--- Naives Bayes.py
+++ Naives Bayes.py
@@ -4,3 +4,6 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 # data or file integration into  the system to train the model 
+data=pd.read_csv('NaivesBayes.csv')
+
+data.head() # to read the header of the  data to understand the target and other columns in it 
