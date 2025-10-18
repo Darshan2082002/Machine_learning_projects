76c303ddbacf98ffc0416c7f6445ef595c98494c
Darshan B <87748126+Darshan2082002@users.noreply.github.com>
2025-10-18 22:33:34 +0530

    Update Naives Bayes.py

--- Naives Bayes.py
+++ Naives Bayes.py
@@ -11,3 +11,4 @@ x="train" # to check the data. if the model work properly
 y=" test" # to predict the model is responding 
 
 (x_train,y_train,x_test,y_test= train_test_split(x,y,random_sixxe=0.2)
+model=LinearRegression 
