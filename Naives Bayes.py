b0a65bed2b43dfbfbe5b52f1f58eadb82309b90a
Darshan2082002 <darshanbalu20@gmail.com>
2025-11-03 21:39:54 +0530

    Naive bayes on spam detection

--- Naives Bayes.py
+++ Naives Bayes.py
@@ -1,22 +1,8 @@
+import matplotlib.pyplot as plt 
 import numpy as np 
 import pandas as pd 
-import matplotlib.pyplot as plt 
+import seaborn as sns 
 from sklearn.model_selection import train_test_split
-from sklearn.linear import LinearRegression #it supervised model which is used to predict the data
-from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB # Naives Bayes implementation 
-from sklearn.metrics import accuracy_score, classification_report # To check the accuracy or the fitting of the data 
-from sklearn.preprocessing import StandardScaler
-# data or file integration into  the system to train the model 
-data=pd.read_csv('NaivesBayes.csv')
-
-data.head() # to read the header of the  data to understand the target and other columns in it 
-x="train" # to check the data. if the model work properly 
-y=" test" # to predict the model is responding 
-
-(x_train,y_train,x_test,y_test= train_test_split(x,y,random_sixxe=0.2)
-model=GaussianNB
-scalar=StrandScalar()
-x_train_scaled=scalar.fit_transform(x_train)
-x_test_scaled=scalar.transform(x_test)
-model.fit(x_train_scaled,y_train)
-y_pred=model.predict(x_test_)
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+from sklearn.preprocessing import LabelEncoder
